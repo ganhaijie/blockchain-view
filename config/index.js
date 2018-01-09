d64cca678bd516d6db454393bb878ae00b42c261
ganhaijie <ganhaijie1028@gmail.com>
2018-01-09 16:26:57 +0800

    axios

--- config/index.js
+++ config/index.js
@@ -10,7 +10,15 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/hound': {
+        target: 'http://127.0.0.1:8082/hound',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/hound': '/'
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
@@ -20,7 +28,7 @@ module.exports = {
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
-    
+
     /**
      * Source Maps
      */
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
     <el-table
-      :data="tableData2"
-      style="width: 100%"
-      :row-class-name="tableRowClassName">
+      :data="coinTable"
+      style="width: 100%">
       <el-table-column header-align="center"
                        prop="platform"
                        label="交易所"
@@ -27,39 +26,28 @@
 </template>
 <script>
   export default {
-    methods: {
-      tableRowClassName({row, rowIndex}) {
-        if (rowIndex === 1) {
-          return 'warning-row';
-        } else if (rowIndex === 3) {
-          return 'success-row';
-        }
-        return '';
-      }
-    },
     data() {
       return {
-        tableData2: [{
-          platform: '火币',
-          coinType: 'BTC',
-          price: '15688',
-          range: '12.5'
-        }, {
-          platform: '火币',
-          coinType: 'ETH',
-          price: '1499',
-          range: '12.2134'
-        }, {
-          platform: '火币',
-          coinType: 'QASH',
-          price: '1246',
-          range: '19.999'
-        }, {
-          platform: '火币',
-          coinType: 'ETC',
-          price: '2234',
-          range: '-2.123'
-        }]
+        coinTable: []
+      }
+    },
+    created() {
+      this.getData();
+    },
+    methods: {
+      getData() {
+        this.search();
+      },
+      search(){
+        this.$axios.get("/hound/coin/v1/getCoinInfo")
+          .then((response)=>{
+            if (response.data.success == true) {
+              this.coinTable=response.data.obj;
+            } else {
+              this.$message.error("信息获取失败");
+            }
+          }).catch((response) => {
+          this.$message.error(response);})
       }
     }
   }
